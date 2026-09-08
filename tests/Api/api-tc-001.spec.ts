27ce92526f2805c23fdc849d224abc2737350212
Pablo <pabloggarron@hotmail.com>
2026-09-07 23:14:30 -0400

    refactor: Borrado de comentarios - Katy

--- tests/Api/api-tc-001.spec.ts
+++ tests/Api/api-tc-001.spec.ts
@@ -1,52 +1,24 @@
-// ============================================================
-// 1. IMPORTAR LAS HERRAMIENTAS DE PLAYWRIGHT
-
 import { test, expect } from "@playwright/test";
 
-// 2. AGRUPAR LAS PRUEBAS DE AUTENTICACIÓN
-
 test.describe("Autenticación mediante API", () => {
+  test("API-TC-001: login válido devuelve HTTP 200 y token", async ({
+    request,
+  }) => {
+    const response = await request.post("/api/auth/login", {
+      data: {
+        username: "admin",
+        password: "admin123",
+      },
+    });
 
-  // 3. CREAR EL CASO DE PRUEBA API-TC-001
+    expect(response.status()).toBe(200);
 
-  test(
-    "API-TC-001: login válido devuelve HTTP 200 y token",
+    const responseBody = await response.json();
 
-    // 4. OBTENER LA HERRAMIENTA "request"
-    async ({ request }) => {
+    expect(responseBody.token).toBeTruthy();
 
-      // 5. ENVIAR LA PETICIÓN DE LOGIN
+    expect(responseBody.user.username).toBe("admin");
 
-      const response = await request.post("/api/auth/login", {
-
-        // 6. ENVIAR LAS CREDENCIALES
-
-        data: {
-
-          username: "admin",
-          password: "admin123",
-        },
-      });
-
-      // 9. VERIFICAR EL CÓDIGO HTTP
-
-      expect(response.status()).toBe(200);
-
-      // 10. OBTENER EL CUERPO DE LA RESPUESTA
-
-      const responseBody = await response.json();
-
-      // 11. VERIFICAR QUE EXISTE UN TOKEN
-
-      expect(responseBody.token).toBeTruthy();
-
-      // 12. VERIFICAR EL NOMBRE DEL USUARIO
-
-      expect(responseBody.user.username).toBe("admin");
-
-      // 13. VERIFICAR QUE EL USUARIO TENGA UN ROL
-
-      expect(responseBody.user.role).toBeTruthy();
-    }
-  );
+    expect(responseBody.user.role).toBeTruthy();
+  });
 });
